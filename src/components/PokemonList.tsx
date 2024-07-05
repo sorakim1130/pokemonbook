b222a9a0deaeb0ee67656ac75648c2e4699440eb
sorakim <sora6418@gmail.com>
2024-07-05 10:18:38 +0900

    Feat: 로딩중 스타일 변경

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -18,20 +18,25 @@ const PokemonList:React.FC = () => {
     }, []);
 
   return (
-    <div className="container ms-auto">
-        PokemonList
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-            {pokemonData.length === 0 ? (<div>로딩</div>) : (pokemonData.map((pokemon) => (
-                <div key={pokemon.id} className="p-4 border rounded-lg">
-                    <Link href={`/pokemon/${pokemon.id}`}>
-                        <Image src={pokemon.sprites.front_default} alt={pokemon.korean_name} width={96} height={96} />
-                        <p>{pokemon.korean_name}</p>
-                        <p>번호 : {pokemon.id}</p>
-                    </Link>
-                </div>
-            )))}
+    <div className="container ms-auto min-h-screen">
+        {pokemonData.length === 0 ? (
+            <div className="flex flex-col justify-center items-center max-h-screen ms-auto">
+                <div className="loader ease-linear rounded-full border-4 border-gray-200 h-12 w-12 mb-10"></div>
+                <p className="text-xl font-bold">로딩중 뀨~.~</p>
+            </div>
+        ) : (
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                {pokemonData.map((pokemon) => (
+            <div key={pokemon.id} className="p-4 border rounded-lg">
+                <Link href={`/pokemon/${pokemon.id}`}>
+                    <Image src={pokemon.sprites.front_default} alt={pokemon.korean_name} width={96} height={96} />
+                    <p>{pokemon.korean_name}</p>
+                    <p>번호 : {pokemon.id}</p>
+                </Link>
+            </div>
+            ))}
         </div>
-
+        )}
     </div>
   )
 }
